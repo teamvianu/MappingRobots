a38641eb10949b629c49c8bbe0be8fff284b5907
Sebastian Ion <sebastian.ion100@gmail.com>
2019-03-11 03:33:42 +0000

    Add dedicated class for map merging

--- src/exercise/part/ros/map_merge.py
+++ src/exercise/part/ros/map_merge.py
@@ -10,12 +10,6 @@ UNKNOWN = -1
 OCCUPIED = 100
 
 
-HEIGHT = None
-WIDTH = None
-
-central_grid = None
-publisher = None
-
 def grid_status(value):
     if value == FREE:
         return 'FREE'
@@ -25,55 +19,122 @@ def grid_status(value):
         return 'OCCUPIED'
 
 
-def decide_cell_occupancy(a, b):
-    if a == b:
-        return a
-    if a == UNKNOWN:
-        return b
-    if b == UNKNOWN:
-        return a
-    # By this point the only options left are (FREE, OCCUPIED) or (OCCUPIED, FREE)
-    # Prefer FREE; might not always be right, but if objects are moving then choosing OCCUPIED can fill the grid completely
-    return FREE
-
-
-def merge_map(map_msg):
-    height, width = map_msg.info.height, map_msg.info.width
-    occupancy_grid = map_msg.data
-
-    global central_grid, HEIGHT, WIDTH
-
-    rospy.loginfo('Received map containing %s', np.unique(occupancy_grid))
-
-    if central_grid is None:
-        HEIGHT = height
-        WIDTH = width
-        rospy.loginfo('Initialized central grid')
-        central_grid = map_msg
-
-    else:
-        # central_grid.info = map_msg.info
-        central_grid.data = [decide_cell_occupancy(x, occupancy_grid[i]) for (i, x) in enumerate(central_grid.data)]
-        # rospy.loginfo('Map at (0,0) is %s', np.array_str(np.array(central_grid.data)))
+# class OccupancyGrid(object):
+#     def __init__(self, msg):
+#         self._load_time = msg.info.map_load_time
+#         self._resolution = msg.info.resolution
+#         self._height = msg.info.height
+#         self._width = msg.info.width
+#         self._origin = msg.info.origin
+#         self._occupancy_grid = np.array(msg.data)
     
-    publish_centralised_map()
+#     @property
+#     def occupancy_grid(self):
+#         return self._occupancy_grid
+
+#     def set_occupancy_grid(self, grid):
+#         self._occupancy_grid = grid
+
+#     @property
+#     def resolution(self):
+#         return self._resolution
+    
+#     @property
+#     def height(self):
+#         return self._height
+    
+#     @property
+#     def width(self):
+#         return self._width
+
+#     @property
+#     def origin(self):
+#         return self._origin
+
+#     @property
+#     def load_time(self):
+#         return self._load_time
+    
+#     def set_load_time(self, time):
+#         self._load_time = time
+
+
+class OccupancyGridMerger(object):
+    def __init__(self, subscribe_topic='map', publish_topic='global_map'):
+        self._occupancy_grid = None
+        self._publisher = rospy.Publisher(publish_topic, OccupancyGrid, queue_size=10)
+        self._subscriber = rospy.Subscriber(subscribe_topic, OccupancyGrid, self.merge_map)
+        self._cell_decision = np.vectorize(self._decide_cell_occupancy)
+    
+    def merge_map(self, msg):
+
+        if self._occupancy_grid == None:
+            rospy.loginfo('Initialized global map')
+            self._occupancy_grid = OccupancyGrid(info=msg.info, data=msg.data)
+        else:
+            # Check that maps are referenced the same way (should be true for current system)
+            assert(self._occupancy_grid.info.resolution == msg.info.resolution)
+            assert(self._occupancy_grid.info.height == msg.info.height)
+            assert(self._occupancy_grid.info.width == msg.info.width)
+            assert(self._occupancy_grid.info.origin == msg.info.origin)
+            
+            self._occupancy_grid.data = self._cell_decision(self._occupancy_grid.data, msg.data)
+            self._occupancy_grid.info.map_load_time = msg.info.map_load_time
+            rospy.loginfo('Global map updated at %s', self._occupancy_grid.info.map_load_time)
+        
+        self._publisher.publish(self._occupancy_grid)
+        rospy.loginfo('Published new global map')
+
+    def _decide_cell_occupancy(self, a, b):
+        if a == b:
+            return a
+        if a == UNKNOWN:
+            return b
+        if b == UNKNOWN:
+            return a
+        return a * b / 100.
+    
+
+# def merge_map(map_msg):
+#     height, width = map_msg.info.height, map_msg.info.width
+#     occupancy_grid = map_msg.data
+
+#     global central_grid, HEIGHT, WIDTH
+
+#     rospy.loginfo('Received map origin %s orientation %s', map_msg.info.origin.position, map_msg.info.origin.orientation)
+
+#     if central_grid is None:
+#         HEIGHT = height
+#         WIDTH = width
+#         rospy.loginfo('Initialized central grid')
+#         central_grid = map_msg
+
+#     else:
+#         # central_grid.info = map_msg.info
+#         central_grid.data = [decide_cell_occupancy(x, occupancy_grid[i]) for (i, x) in enumerate(central_grid.data)]
+#         # rospy.loginfo('Map at (0,0) is %s', np.array_str(np.array(central_grid.data)))
+    
+#     publish_centralised_map()
 
 
 
-def listener():
-    rospy.init_node('map_listener', anonymous=True)
+# def listener():
+#     rospy.init_node('map_listener', anonymous=True)
 
-    rospy.Subscriber('map', OccupancyGrid, merge_map)
+#     rospy.Subscriber('map', OccupancyGridMsg, merge_map)
 
-    rospy.spin()
+#     rospy.spin()
 
 
-def publish_centralised_map():
-    publisher.publish(central_grid)
+# def publish_centralised_map():
+#     publisher.publish(central_grid)
 
 
 
 if __name__ == "__main__":
-    global publisher
-    publisher = rospy.Publisher('central_map', OccupancyGrid, queue_size=10)
-    listener()
+    # global publisher
+    # publisher = rospy.Publisher('central_map', OccupancyGridMsg, queue_size=10)
+    # listener()
+    rospy.init_node('mapMerger')
+    merger = OccupancyGridMerger('map', 'global_map')
+    rospy.spin()
