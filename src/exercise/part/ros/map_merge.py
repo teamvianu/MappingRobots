13cfcc747012e23e8df27e95ea4625c3e8a53e00
Sebastian Ion <sebastian.ion100@gmail.com>
2019-03-11 03:35:57 +0000

    Remove commented code in map merger

--- src/exercise/part/ros/map_merge.py
+++ src/exercise/part/ros/map_merge.py
@@ -19,46 +19,6 @@ def grid_status(value):
         return 'OCCUPIED'
 
 
-# class OccupancyGrid(object):
-#     def __init__(self, msg):
-#         self._load_time = msg.info.map_load_time
-#         self._resolution = msg.info.resolution
-#         self._height = msg.info.height
-#         self._width = msg.info.width
-#         self._origin = msg.info.origin
-#         self._occupancy_grid = np.array(msg.data)
-    
-#     @property
-#     def occupancy_grid(self):
-#         return self._occupancy_grid
-
-#     def set_occupancy_grid(self, grid):
-#         self._occupancy_grid = grid
-
-#     @property
-#     def resolution(self):
-#         return self._resolution
-    
-#     @property
-#     def height(self):
-#         return self._height
-    
-#     @property
-#     def width(self):
-#         return self._width
-
-#     @property
-#     def origin(self):
-#         return self._origin
-
-#     @property
-#     def load_time(self):
-#         return self._load_time
-    
-#     def set_load_time(self, time):
-#         self._load_time = time
-
-
 class OccupancyGridMerger(object):
     def __init__(self, subscribe_topic='map', publish_topic='global_map'):
         self._occupancy_grid = None
@@ -93,48 +53,9 @@ class OccupancyGridMerger(object):
         if b == UNKNOWN:
             return a
         return a * b / 100.
-    
-
-# def merge_map(map_msg):
-#     height, width = map_msg.info.height, map_msg.info.width
-#     occupancy_grid = map_msg.data
-
-#     global central_grid, HEIGHT, WIDTH
-
-#     rospy.loginfo('Received map origin %s orientation %s', map_msg.info.origin.position, map_msg.info.origin.orientation)
-
-#     if central_grid is None:
-#         HEIGHT = height
-#         WIDTH = width
-#         rospy.loginfo('Initialized central grid')
-#         central_grid = map_msg
-
-#     else:
-#         # central_grid.info = map_msg.info
-#         central_grid.data = [decide_cell_occupancy(x, occupancy_grid[i]) for (i, x) in enumerate(central_grid.data)]
-#         # rospy.loginfo('Map at (0,0) is %s', np.array_str(np.array(central_grid.data)))
-    
-#     publish_centralised_map()
-
-
-
-# def listener():
-#     rospy.init_node('map_listener', anonymous=True)
-
-#     rospy.Subscriber('map', OccupancyGridMsg, merge_map)
-
-#     rospy.spin()
-
-
-# def publish_centralised_map():
-#     publisher.publish(central_grid)
-
 
 
 if __name__ == "__main__":
-    # global publisher
-    # publisher = rospy.Publisher('central_map', OccupancyGridMsg, queue_size=10)
-    # listener()
     rospy.init_node('mapMerger')
     merger = OccupancyGridMerger('map', 'global_map')
     rospy.spin()
